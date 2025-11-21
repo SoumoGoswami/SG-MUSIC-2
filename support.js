0faf76858e4b9bfbe746b73a60a687dc2f2c31ff
SoumoGoswami <134261542+SoumoGoswami@users.noreply.github.com>
2025-11-21 13:22:09 +0600

    Add files via upload

---
+++ support.js
@@ -0,0 +1,49 @@
+const { EmbedBuilder } = require('discord.js');
+const config = require("../config.js");
+const musicIcons = require('../UI/icons/musicicons.js');
+
+module.exports = {
+    name: "support",
+    description: "Get support server link",
+    permissions: "0x0000000000000800",
+    options: [],
+    run: async (client, interaction, lang) => {
+        try {
+            const supportServerLink = "https://discord.gg/";
+            const githubLink = "https://github.com/";
+            const replitLink = "https://replit.com/";
+            const youtubeLink = "https://www.youtube.com/";
+
+            const embed = new EmbedBuilder()
+                .setColor('#b300ff')
+                .setAuthor({
+                    name: lang.support.embed.authorName,
+                    iconURL: musicIcons.beats2Icon, 
+                    url: config.SupportServer
+                })
+                .setDescription(lang.support.embed.description
+                    .replace("{supportServerLink}", supportServerLink)
+                    .replace("{githubLink}", githubLink)
+                    .replace("{replitLink}", replitLink)
+                    .replace("{youtubeLink}", youtubeLink)
+                )
+                .setImage('https://cdn.discordapp.com/attachments/1393509889904607255/1441210795076620378/InShot_20251110_221452599.gif?ex=6920f7a5&is=691fa625&hm=64cd2e537b609bdfa54c07acfb4876e58c102ae040d76d466a7b9b8db2700c8e&')
+                .setTimestamp();
+
+            await interaction.reply({ embeds: [embed] });
+        } catch (e) {
+            console.error(e);
+            const errorEmbed = new EmbedBuilder()
+                .setColor('#ff0000')
+                .setAuthor({
+                    name: lang.support.embed.error,
+                    iconURL: musicIcons.alertIcon,
+                    url: config.SupportServer
+                })
+                .setDescription(lang.support.embed.errorDescription)
+                .setFooter({ text: lang.footer, iconURL: musicIcons.heartIcon });
+
+            await interaction.reply({ embeds: [errorEmbed], ephemeral: true });
+        }
+    },
+};
